afcbb21f924dee1fe57a03a0dceddf5d2aad3a0f
Sheharyar-1 <sheharyar.ahmed@xprolabs.com>
2024-07-30 19:55:56 +0500

    CRUD ajax

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,7 @@
+// app/javascript/packs/application.js
+import Rails from "@rails/ujs"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+ActiveStorage.start()
